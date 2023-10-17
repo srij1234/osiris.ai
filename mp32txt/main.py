c41ebffd8b38540537e87881e441c47f3b8dbb8e
Srijan Sehdev <srij123451@gmail.com>
2023-10-17 17:45:09 +0530

    new file:   mp32txt/__init__.py 	modified:   mp32txt/main.py 	new file:   yt2txt/__init__.py 	new file:   yt2txt/main.py

--- mp32txt/main.py
+++ mp32txt/main.py
@@ -1,8 +1,17 @@
 import whisper
 
-model = whisper.load_model("base")
-text = model.transcribe("mp32txt/test.mp3")['text']
-# text=text[text]
+def transcribe_audio(input_file, output_file):
+    model = whisper.load_model("base")
+    text = model.transcribe(input_file)['text']
+    
+    with open(output_file, "w", encoding='utf-8') as f:
+        f.write(text)
 
-with open("mp32txt/text.txt","w",encoding='utf-8') as f:
-    f.write(text)
+def main():
+    input_file = "mp32txt/test.mp3"
+    output_file = "mp32txt/text.txt"
+    
+    transcribe_audio(input_file, output_file)
+
+if __name__ == "__main__":
+    main()
